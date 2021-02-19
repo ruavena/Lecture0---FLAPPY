7ad6868b14d5728babe037af1093a4c1e4ea7b55
ruavena <72678746+ruavena@users.noreply.github.com>
2021-02-19 19:31:38 +0800

    Add files via upload

---
+++ FLAPPY-3/bird.lua
@@ -0,0 +1,26 @@
+Bird = Class{}
+
+Gravity = 25
+
+function Bird:init()
+   
+    	self.image = love.graphics.newImage('bird.png')
+    	self.width = self.image:getWidth()
+    	self.height = self.image:getHeight()
+
+    
+    	self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    	self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+	self.dy = 0
+end
+
+function Bird:update(dt)
+    
+    	self.dy = self.dy + Gravity * dt
+    	self.y = self.y + self.dy
+end
+
+function Bird:render()
+    	love.graphics.draw(self.image, self.x, self.y)
+end
---
+++ FLAPPY-8/main.lua
@@ -0,0 +1,111 @@
+push = require 'push'
+
+Class = require 'class'
+
+require 'pipe'
+
+require 'bird'
+
+require 'pipepair'
+
+require 'StateMachine'
+require 'states/BaseState'
+require 'states/PlayState'
+require 'states/TitleScreenState'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local background = love.graphics.newImage('Background.png')
+local backgroundScroll = 0
+
+local ground = love.graphics.newImage('Ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
+
+local GROUND_LOOPING_POINT = 514
+
+local bird = Bird()
+
+local pipePairs = {}
+
+local spawnTimer = 0
+
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
+
+local scrolling = true
+
+function love.load()
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+
+	love.window.setTitle('Pogyu Bird')
+
+    	smallFont = love.graphics.newFont('font.ttf', 8)
+    	mediumFont = love.graphics.newFont('flappy.ttf', 14)
+    	flappyFont = love.graphics.newFont('flappy.ttf', 28)
+    	hugeFont = love.graphics.newFont('flappy.ttf', 56)
+    	love.graphics.setFont(flappyFont)
+
+	
+	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {vsync = true, fullscreen = false, resizable = true})
+
+	gStateMachine = StateMachine {
+        	['title'] = function() return TitleScreenState() end,
+        	['play'] = function() return PlayState() end,
+	}
+   	gStateMachine:change('title')
+
+	love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+   	push:resize(w, h)
+end
+
+function love.keypressed(key)
+
+	love.keyboard.keysPressed[key] = true
+	
+   	if key == 'escape' then
+   		love.event.quit()
+  	end
+end
+
+function love.keyboard.wasPressed(key)
+	if love.keyboard.keysPressed[key] then
+        	return true
+    	else
+        	return false
+    	end
+end
+
+function love.update(dt)
+	
+    	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+
+	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
+
+	gStateMachine:update(dt)
+		
+
+	love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    	push:start()
+
+    	love.graphics.draw(background, -backgroundScroll, 0)
+
+	gStateMachine:render()
+
+    	love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+	
+	push:finish()
+end
